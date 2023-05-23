ff8e6622fddc1e4d81243613e34af0d11d6f5eb4
Eduardo Henrique <edu.hen.fm@gmail.com>
2023-05-23 07:00:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,13 +7,41 @@ sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update -y && sudo apt upgrade -y
 
 # Install the dependencies
-sudo apt-get install -y git build-essential curl grep python3.11
+sudo apt-get install -y git build-essential curl grep python3.11 zsh tmux cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+
+# Change default shell to zsh
+chsh -s $(which zsh)
+
+# Install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 
 # Install rust and cargo
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install alacritty
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+cargo build --release
+infocmp alacritty
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+sudo mkdir -p /usr/local/share/man/man1
+gzip -c extra/alacritty.man | sudo tee /usr/local/share/man/man1/alacritty.1.gz > /dev/null
+gzip -c extra/alacritty-msg.man | sudo tee /usr/local/share/man/man1/alacritty-msg.1.gz > /dev/null
+mkdir -p ${ZDOTDIR:-~}/.zsh_functions
+cp extra/completions/_alacritty ${ZDOTDIR:-~}/.zsh_functions/_alacritty
+
+# Change default terminal emulator to alacritty
+sudo update-alternatives --install /usr/bin/x-terminal-emulator x-terminal-emulator /usr/local/bin/alacritty 50
+gsettings set org.gnome.desktop.default-applications.terminal exec '/usr/local/bin/alacritty'
 
 # Install node and npm
 nvm install node
@@ -25,4 +53,12 @@ tar xf lazygit.tar.gz lazygit
 sudo install lazygit /usr/local/bin
 
 # Install lunarvim
-LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh) -y
+
+# Move config files
+cd ..
+cp .zshrc ~/.zshrc
+cp .tmux.conf ~/.tmux.conf
+cp ./lvim/config.lua ~/.config/lvim/config.lua
+cp alacritty.yml ~/.config/alacritty/alacritty.yml
+cp .gitconfig ~/.gitconfig
